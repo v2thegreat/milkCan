dc689b079d3d781a8a571d3600bf31ee9ab0794a
v2thegreat <aliabbaszaidi@outlook.com>
2018-04-23 14:23:10 -0400

    Updated Version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='milkCan',
-      version='0.1',
+      version='0.11',
       description='A simple python library to compare runtime of several functions at once',
       url='https://github.com/v2thegreat/milkCan',
       author='v2thegreat',
