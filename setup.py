6e98df6f2b18c56aced308bae6b14ebdc900db3e
v2thegreat <aliabbaszaidi@outlook.com>
2018-04-23 17:57:10 -0400

    Refactored

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='milkCan',
-      version='0.11',
-      description='A simple python library to compare runtime of several functions at once',
-      url='https://github.com/v2thegreat/milkCan',
-      author='v2thegreat',
-      author_email='v2thegreat@gmail.com',
-      license='MIT',
-      packages=['milkCan'],
-      zip_safe=False,
-      dependency_links=['']
-      )
+	version='0.11',
+	description='A simple python library to compare runtime of several functions at once',
+	url='https://github.com/v2thegreat/milkCan',
+	author='v2thegreat',
+	author_email='v2thegreat@gmail.com',
+	license='MIT',
+	packages=['milkCan'],
+	zip_safe=False,
+	dependency_links=['https://github.com/v2thegreat/quickTests']
+	)
