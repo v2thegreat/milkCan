c3c887a297707671b98cea0253e518083ac2e37a
v2thegreat <aliabbaszaidi@outlook.com>
2018-04-23 14:18:49 -0400

    Added Plotting functionality

--- milkCan/quickTests/quickTests.py
+++ milkCan/quickTests/quickTests.py
@@ -5,9 +5,10 @@ import warnings
 
 try:
     import matplotlib.pyplot as plt
-
+    __MATPLOTLIB_INSTALLED__ = True
 except ImportError:
     warnings.warn("Matplotlib not Found", ImportWarning)
+    __MATPLOTLIB_INSTALLED__ = False
 
 
 class quickTests:
@@ -16,16 +17,14 @@ class quickTests:
 
         :type iterable: Collection
         """
-        self.__meanLst = []
-        self.__speedTestRunTimes = []
         self.iterable = iterable
         self.functions = []
+        self.__meanLst = []
+        self.__speedTestRunTimes = []
+        self.__function_names = []
 
-        for x in functions:
-            if not callable(x):
-                raise ValueError("{} is not a callable function".format(x))
-
-            self.functions.append(x)
+        self._setFunctions(functions)
+        self._setFunctionNames()
 
     def run(self):
         for func in self.functions:
@@ -45,9 +44,34 @@ class quickTests:
         min_pos = self.__meanLst.index(min_time)
         return self.functions[min_pos]
 
+    def getPlot(self):
+        self._checkMatplotlib()
+
+        for pos, runTimes in enumerate(self.__speedTestRunTimes):
+            plt.plot(runTimes, label = self.__function_names[pos])
+
+        return plt
+
+
+    def _setFunctionNames(self):
+        for x in self.functions:
+            self.__function_names.append(str(x).split()[1])
+
+    def _setFunctions(self, functions):
+        for x in functions:
+            if not callable(x):
+                raise ValueError("{} is not a callable function".format(x))
+
+            self.functions.append(x)
+
     @property
     def _get_all_mean_time(self) -> List:
         return [self._mean(times) for times in self.__speedTestRunTimes]
+
+    @staticmethod
+    def _checkMatplotlib():
+        if not __MATPLOTLIB_INSTALLED__:
+            raise ImportError("Matplotlib not Installed")
 
     @staticmethod
     def _mean(times):
